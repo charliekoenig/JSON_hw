c846ca79276bca71e5cdd47c3705ef7756e0c8d2
Charlie Koenig <65825604+charliekoenig@users.noreply.github.com>
2024-07-09 11:41:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-function getData() {
-    index = document.getElementById("siteNames").selectedIndex;
-	website = document.getElementById("siteNames").options[index].value.toLowerCase();
-    url = "https://www." + website + ".com/";
+i = 0;
 
-    response = fetch("https://api.websitecarbon.com/site?url=" + url)
+function parseData() {
+    word = document.getElementById("word").value;
+    word = word.split(' ')[0];
+    console.log(word);
+	
+    url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+
+    definition = document.getElementById("def");
+    partOfSpeech = document.getElementById("pS");
+
+    definitionText = "<strong>Definition: </strong>"
+    partOfSpeechText = "<strong>Part of Speech: </strong>"
+
+    response = fetch(url)
         .then((response) => {
             return response.json()
         })
         .then((jsonData) => {
-            console.log(jsonData);
+            if (i >= jsonData[0].meanings.length) {
+                i = jsonData[0].meanings.length - 1;
+            }
+            definition.innerHTML = definitionText + jsonData[0].meanings[i].definitions[0].definition;
+            partOfSpeech.innerHTML = partOfSpeechText + jsonData[0].meanings[i].partOfSpeech;
         });
+}
+
+function next() {
+    i += 1;
+    parseData();
+}
+
+
+function prev() {
+    i > 0? i -= 1 : i=0;
+    parseData();
 }
